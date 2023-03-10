2e22d26234901feea19815882ec81fe741c47212
Aslan Sarı <aslan.sari@loodos.com>
2023-03-10 09:53:08 +0300

    feat: add base view model and related docs

--- app/src/main/java/com/loodos/samplecomposeandroid/MainActivity.kt
+++ app/src/main/java/com/loodos/samplecomposeandroid/MainActivity.kt
@@ -67,6 +67,7 @@ class MainActivity : ComponentActivity() {
                 }.start()
         }
     }
+
     private fun setContent() {
         setContent {
             SampleComposeAndroidTheme {
